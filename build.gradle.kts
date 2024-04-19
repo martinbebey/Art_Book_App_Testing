1209836ae94dc26d673f1511fda22b617cab327b
Martin Bebey <37882687+martinbebey@users.noreply.github.com>
2024-04-19 19:45:05 -0400

    added dependencies + first successful run

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,15 @@
+import org.jetbrains.kotlin.fir.declarations.builder.buildScript
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies{
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
+    }
+}
+
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("com.google.devtools.ksp") version "1.9.21-1.0.15" apply false
 }
